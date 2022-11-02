0be94a0cf1e36e9fbd3eecd0f3939a8e3dd194fe
David Hanna <david.hanna@kickstepinc.com>
2022-11-02 19:43:59 -0400

    testing workspace patch

---
+++ packages/create-docker-baseline/createDockerBaseline.js
@@ -0,0 +1,11 @@
+'use strict';
+
+const chalk = require('chalk');
+const commander = require('commander');
+const envinfo = require('envinfo');
+
+const packageJson = require('./package.json');
+
+function init() {
+    console.log('Called createDockerBaseline.js');
+}
